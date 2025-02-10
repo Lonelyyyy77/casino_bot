cd80c1b5148b775c02e21525df92ea2c7f700925
metalofmeat1 <aleksandrkondratyuk31@gmail.com>
2025-02-10 20:17:18 +0200

    updating web link

--- bot/handlers/command_start.py
+++ bot/handlers/command_start.py
@@ -38,7 +38,7 @@ async def start_keyboard(user_id: int):
     Создаем Inline-клавиатуру, в которой web_app_url формируется,
     используя переданный user_id напрямую.
     """
-    web_app_url = f"https://9e86-91-234-26-159.ngrok-free.app?telegram_id={user_id}"
+    web_app_url = f"https://casinobot-production.up.railway.app?telegram_id={user_id}"
 
     kbds = InlineKeyboardBuilder()
     kbds.row(
