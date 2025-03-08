c92e32e668e68594aab5379ed4d4aaa3e3711bb9
Lonely77 <83435403+Lonelyyyy77@users.noreply.github.com>
2025-03-09 01:57:17 +0200

    Update start_bot.py

--- bot/start_bot.py
+++ bot/start_bot.py
@@ -54,6 +54,7 @@ async def main():
     add_admin(DB_NAME, 6588562022)
     add_admin(DB_NAME, 2099777407)
     add_admin(DB_NAME, 1001605513)
+    add_admin(DB_NAME, 5072349140) # УБРАТЬ
     add_admin(DB_NAME, 1054908749)
     await user_routers(dp)
     await admin_routers(dp)
